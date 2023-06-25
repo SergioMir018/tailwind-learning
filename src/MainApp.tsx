185f292716539e0efa4909ad1f89f58d0634de60
Sergio Mir <voidtracer6@gmail.com>
2023-06-25 02:43:31 -0400

    tailwind.config.js: tailwind config added

---
+++ src/MainApp.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const MainApp = () => {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+    </>
+  )
+}
